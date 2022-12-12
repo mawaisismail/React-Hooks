bf6e467d64466e1df65406031ada8fb4d2429f56
talk2awais <muhammad.awais.77831@gmail.com>
2022-12-12 19:28:12 +0500

    Integrate useId hook

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,9 +1,12 @@
 import { Bio } from "./bio";
+import { useId } from "react";
 
 export const About = () => {
+  const id = useId();
   return (
     <div>
       <p>About</p>
+      <p>ID1 {id}</p>
       <Bio />
     </div>
   );
